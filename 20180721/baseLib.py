ce80f782821f16ecac701fea804fab8fbe090f3c
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-07-23 23:34:19 +0800

    update

--- 20180721/baseLib.py
+++ 20180721/baseLib.py
@@ -312,15 +312,15 @@ from requests import Request, Session
 
 
 # requests test -> prepare_request
-url = 'http://httpbin.org/post'
-data = {
-    'name': 'Sniper'
-}
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-}
-s = Session()
-req = Request('POST', url, data=data, headers=headers)
-prepped = s.prepare_request(req)
-r = s.send(prepped)
-print(r.text)
+# url = 'http://httpbin.org/post'
+# data = {
+#     'name': 'Sniper'
+# }
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+# }
+# s = Session()
+# req = Request('POST', url, data=data, headers=headers)
+# prepped = s.prepare_request(req)
+# r = s.send(prepped)
+# print(r.text)
