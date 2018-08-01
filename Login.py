1c4e3dc7899f17e2737706bb29d8e62189853894
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-01 14:17:51 +0800

    update

---
+++ Login.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import requests
+from lxml import etree
+
+class Login():
+    def __init__(self):
+        self.headers = {
+            'referer': 'https://github.com/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Host': 'github.com'
+        }
+        self.login_url = 'https://github.com/login'
+        self.post_url = 'https://github.com/session'
+        self.logined_url = 'https://github.com/settings/profile'
+        self.session = requests.Session()
+
