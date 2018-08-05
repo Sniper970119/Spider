dc35ebdf8bf88fe4e250b4e6d56c56c65edf5a37
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-05 21:06:06 +0800

    update

--- ScrapyTest/tutorial/tutorial/settings.py
+++ ScrapyTest/tutorial/tutorial/settings.py
@@ -43,8 +43,8 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+#   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+#   'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
 # }
 
 # Enable or disable spider middlewares
---
+++ ScrapyTest/tutorial/tutorial/spiders/zhihu.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    custom_settings = {
+        'DEFAULT_REQUEST_HEADERS': {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        }
+    }
+
+    def parse(self, response):
+        pass
