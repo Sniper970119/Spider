460cc6ea6e30b1c538b7ad50fc86e7ec240abf28
Zhao <30463691+Sniper970119@users.noreply.github.com>
2018-08-08 22:30:23 +0800

    update

---
+++ ScrapyTest/tutorial/tutorial/spiders/google.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GoogleSpider(scrapy.Spider):
+    name = 'google'
+    allowed_domains = ['www.google.com']
+    start_urls = ['http://www.google.com/']
+
+
+
+    def parse(self, response):
+        print()
